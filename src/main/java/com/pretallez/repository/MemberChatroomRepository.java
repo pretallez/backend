5f6e7c65645221645022665c2b6aace1ca27cd5b
disking12@naver.com <disking12@naver.com>
2025-03-08 19:28:24 +0900

    feat: 회원 채팅방 조회 QueryDSL 메서드 생성 (#14)

--- src/main/java/com/pretallez/repository/MemberChatroomRepository.java
+++ src/main/java/com/pretallez/repository/MemberChatroomRepository.java
@@ -1,5 +1,6 @@
 package com.pretallez.repository;
 
+import com.pretallez.model.dto.memberchatroom.MemberChatroomRead;
 import com.pretallez.model.entity.Chatroom;
 import com.pretallez.model.entity.Member;
 import com.pretallez.model.entity.MemberChatroom;
@@ -15,5 +16,8 @@ public interface MemberChatroomRepository {
 
     /** Query Method */
     Optional<MemberChatroom> findByMemberAndChatroom(Member member, Chatroom chatroom);
-    List<MemberChatroom> findByMember(Member member);
+
+    /** QueryDSL */
+    /** QueryDSL을 사용하여 MemberChatroom 엔티티와 관련된 Chatroom, VotePost, Board 테이블을 조인하여 한 번의 쿼리로 데이터를 가져온다. */
+    List<MemberChatroomRead.Response> findByMemberWithChatroomAndBoard(Long memberId);
 }
