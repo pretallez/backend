c0129692595b9d5e0b944ba4b1004a1765c1ff89
disking12@naver.com <disking12@naver.com>
2025-03-08 19:37:44 +0900

    fire: 레포지토리 테스트 코드 삭제 및 Example 삭제 (#14)

--- src/main/java/com/pretallez/repository/jpa/ExampleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pretallez.repository.jpa;
-
-import com.pretallez.model.entity.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExampleJpaRepository extends JpaRepository<Example,Long> {
-}
--- src/test/java/com/pretallez/common/fixture/Fixture.java
+++ src/test/java/com/pretallez/common/fixture/Fixture.java
@@ -1,7 +1,6 @@
 package com.pretallez.common.fixture;
 
 import com.pretallez.model.dto.chatroom.ChatroomCreate;
-import com.pretallez.model.dto.example.ExampleCreate;
 import com.pretallez.model.dto.memberchatroom.MemberChatroomCreate;
 import com.pretallez.model.dto.memberchatroom.MemberChatroomDelete;
 import com.pretallez.model.dto.memberchatroom.MemberChatroomRead;
@@ -12,13 +11,6 @@ import com.pretallez.model.enums.MannerLevel;
 import java.time.LocalDateTime;
 
 public class Fixture {
-
-    public static Example example() {
-        String fakeName = "김성호";
-        String fakeNickName = "ksngh";
-        ExampleCreate.Request request = ExampleCreate.Request.of(fakeName, fakeNickName);
-        return Example.create(request);
-    }
 
     public static Chatroom chatroom(VotePost votePost) {
         return Chatroom.of(votePost);
