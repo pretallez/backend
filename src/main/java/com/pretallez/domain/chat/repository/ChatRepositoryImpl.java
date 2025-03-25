64eb8cf014cc2ed12b07b8163a5226124cb9807f
disking12@naver.com <disking12@naver.com>
2025-03-25 12:40:40 +0900

    feat: 채팅 내역 조회 레포지토리 코드 구현 (#22)

--- src/main/java/com/pretallez/domain/chat/repository/ChatRepositoryImpl.java
+++ src/main/java/com/pretallez/domain/chat/repository/ChatRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.pretallez.domain.chat.repository;
 
+import static com.pretallez.common.entity.QChat.*;
+
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import com.pretallez.common.entity.Chat;
+import com.pretallez.domain.chat.dto.ChatQueryRequest;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -29,5 +33,29 @@ public class ChatRepositoryImpl implements ChatRepository{
 	@Override
 	public List<Chat> findAll() {
 		return chatJpaRepository.findAll();
+	}
+
+	@Override
+	public List<Chat> findNextChats(ChatQueryRequest chatQueryRequest) {
+		return queryFactory
+			.selectFrom(chat)
+			.where(buildPaginationCondition(chatQueryRequest))
+			.orderBy(chat.createdAt.desc(), chat.id.desc())
+			.limit(chatQueryRequest.getLimit())
+			.fetch();
+	}
+
+	private BooleanExpression buildPaginationCondition(ChatQueryRequest chatQueryRequest) {
+		// 초기 조회 시 모든 데이터 대상
+		if (chatQueryRequest.getLastCreatedAt() == null) {
+			return null;
+		}
+
+		return chat.chatroomId.eq(chatQueryRequest.getChatroomId())
+			.and(
+				chat.createdAt.lt(chatQueryRequest.getLastCreatedAt())
+					.or(chat.createdAt.eq(chatQueryRequest.getLastCreatedAt())
+						.and(chat.id.lt(chatQueryRequest.getLastId())))
+			);
 	}
 }
