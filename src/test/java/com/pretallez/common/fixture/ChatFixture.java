453da092f3a156f13201a53dfde8d6e2910882ed
disking12@naver.com <disking12@naver.com>
2025-03-25 12:44:02 +0900

    feat: 채팅 내역 조회 컨트롤러 구현 및 테스트 (#22)

--- src/test/java/com/pretallez/common/fixture/ChatFixture.java
+++ src/test/java/com/pretallez/common/fixture/ChatFixture.java
@@ -1,6 +1,7 @@
 package com.pretallez.common.fixture;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -8,6 +9,7 @@ import com.pretallez.common.entity.Chat;
 import com.pretallez.common.entity.MemberChatroom;
 import com.pretallez.common.enums.MessageType;
 import com.pretallez.domain.chat.dto.ChatCreate;
+import com.pretallez.domain.chat.dto.ChatQueryRequest;
 import com.pretallez.domain.chat.dto.ChatRead;
 
 public class ChatFixture {
@@ -28,11 +30,27 @@ public class ChatFixture {
 		return new ChatCreate.Request(1L, 1L, "Hello", MessageType.CHAT);
 	}
 
+	public static ChatCreate.Response chatCreateResponse() {
+		return new ChatCreate.Response(1L, 1L, "testUser", "Hello", MessageType.CHAT, LocalDateTime.now());
+	}
+
 	public static ChatCreate.Request chatCreateReqeustWithMemberChatroom(MemberChatroom memberChatroom) {
 		return new ChatCreate.Request(memberChatroom.getMember().getId(), memberChatroom.getChatroom().getId(), "Hello", MessageType.CHAT);
 	}
 
 	public static ChatRead.Response chatReadResponse() {
 		return new ChatRead.Response(1L, 1L, 1L, "testUser", "Hello", MessageType.CHAT, LocalDateTime.now());
+	}
+
+	public static ChatQueryRequest chatQueryRequest() {
+		return new ChatQueryRequest(1L, LocalDateTime.now(), 4L, 20);
+	}
+
+	public static List<ChatRead.Response> chatReadResponses() {
+		return List.of(
+			new ChatRead.Response(3L, 1L, 1L, "임종엽", "채팅내용3", MessageType.CHAT, LocalDateTime.of(2025, 3, 24, 11, 7)),
+			new ChatRead.Response(2L, 1L, 2L, "김성호", "채팅내용2", MessageType.CHAT, LocalDateTime.of(2025, 3, 24, 11, 6)),
+			new ChatRead.Response(1L, 1L, 1L, "임종엽", "채팅내용1", MessageType.CHAT, LocalDateTime.of(2025, 3, 24, 11, 5))
+		);
 	}
 }
