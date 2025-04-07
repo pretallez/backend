b9e357e513f3d39fad778e8f7328ed2e94d2bca2
ksngh <chzhqk98@naver.com>
2025-04-07 13:32:01 +0900

    feat : 인증 및 인가 handler 작성 및 configuration (#21)

--- src/main/java/com/pretallez/common/config/SecurityConfig.java
+++ src/main/java/com/pretallez/common/config/SecurityConfig.java
@@ -1,18 +1,16 @@
 package com.pretallez.common.config;
 
 import com.pretallez.common.filter.JwtAuthenticationFilter;
+import com.pretallez.common.security.JwtAccessDeniedHandler;
 import com.pretallez.common.security.JwtAuthenticationEntryPoint;
 import com.pretallez.common.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,32 +21,24 @@ public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider);
 
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/signup", "/api/login").permitAll()
+                        .requestMatchers("/v1/api/join", "/v1/api/login").permitAll()
                         .anyRequest().authenticated()
                 )
-
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
                 )
-
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/pretallez/common/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/pretallez/common/security/JwtAuthenticationEntryPoint.java
@@ -1,8 +1,12 @@
 package com.pretallez.common.security;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pretallez.common.response.CustomApiResponse;
+import com.pretallez.common.response.error.ResErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,20 +14,23 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
+
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
 
-        response.getWriter().write("""
-            {
-              "message": "인증이 필요합니다.",
-              "error": "Unauthorized",
-              "status": 401
-            }
-        """);
+        CustomApiResponse<Void> errorBody = CustomApiResponse.ERROR(ResErrorCode.UNAUTHORIZED);
+
+        String json = objectMapper.writeValueAsString(errorBody);
+        response.getWriter().write(json);
     }
 }
+
