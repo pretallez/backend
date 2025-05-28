3b354395ab4acbef0a39ae0d768a6139f14b30b5
종엽 임 <jylim@itsconv.com>
2025-05-28 14:14:21 +0900

    feat: 닉네임 캐시 조회 DB fallback 로직 구현 (#35)

--- src/main/java/com/pretallez/domain/member/repository/MemberRepository.java
+++ src/main/java/com/pretallez/domain/member/repository/MemberRepository.java
@@ -1,12 +1,15 @@
 package com.pretallez.domain.member.repository;
 
-import com.pretallez.domain.member.entity.Member;
-
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+
+import com.pretallez.domain.member.entity.Member;
 
 public interface MemberRepository {
 
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByEmail(String username);
+    Map<Long, String> findNicknamesByIds(Set<Long> ids);
 }
